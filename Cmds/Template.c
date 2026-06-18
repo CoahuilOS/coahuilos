b8c40d93b1509086d482497e5664c512f4b00841
Willaw-Corona <cobotl@proton.me>
2026-06-18 17:19:47 -0600

    Desarollo del template de comandos [Incompleto]

---
+++ Cmds/Template.c
@@ -0,0 +1,33 @@
+// Template de comando
+#include <stdint.h>
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+
+extern unsigned int INPUT_MAX;
+extern u8 color;
+extern unsigned int cy;
+extern unsigned int cx;
+extern void print(const char *s);
+extern void perror(const char *s);
+extern void clear(void);
+extern void readline(char *buf, int max);
+extern int strcmp(const char *a, const char *b);
+extern void draw_tb(void);
+extern void run_script(const char *path);
+
+int temp(void)
+{
+
+    char line[INPUT_MAX];
+    char *a;
+    char *b;
+
+    color = 0x1F;
+    u8 oldcolor = color;
+    print("         Menu de ayuda de CoahuilOS");
+    color = 0x1D;
+    print("\n");
+    print("Controles y Atajos [Controles]\n");
+}
