b19900bbde7f386fd07f11b7d6c61ae152865bf3
Willaw-Corona <cobotl@proton.me>
2026-06-18 17:25:43 -0600

    Desarollo del template de comandos [Incompleto]

--- Cmds/Template.c
+++ Cmds/Template.c
@@ -29,5 +29,17 @@ int temp(void)
     print("         Menu de ayuda de CoahuilOS");
     color = 0x1D;
     print("\n");
-    print("Controles y Atajos [Controles]\n");
+    print("Controles y Atajos [Controls]\n");
+    print("Sobre CoahuilOS [Abt]\n");
+    print("Comando de ayuda [Cmds]\n");
+    print("Keymap [KM]\n");
+    print("Ubicacion de Memoria [Addr]\n");
+    print("F.A.Q [FAQ]\n");
+    color = oldcolor;
+    for(;;)
+    {
+
+        draw_tb();
+        color = oldcolor;
+    }
 }
